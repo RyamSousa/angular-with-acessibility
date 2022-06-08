9fb173b300a45dbd9ed51aac6b48eb5fa112a515
RyamSousa <ryam.sousa123@gmail.com>
2022-06-08 11:43:22 -0300

    :sparkles: added 'yes-no-button-group' module and component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { FormControl, FormGroup } from "@angular/forms";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+	selector: "app-root",
+	templateUrl: "./app.component.html",
+	styleUrls: ["./app.component.scss"],
 })
-export class AppComponent {
-  title = 'a11y-p1';
+export class AppComponent implements OnInit {
+	title = "Curso Acessibilidade";
+	public formGroup: FormGroup = null;
+
+	ngOnInit(): void {
+		this.formGroup = new FormGroup({
+			yesNoAnswer: new FormControl("no"),
+		});
+	}
+
+	public submit(): void {
+		console.log(this.formGroup.value);
+	}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,14 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
+import { YesNoButtonGroupModule } from "./shared/components/yes-no-button-group/yes-no-button-group.module";
+import { ReactiveFormsModule } from "@angular/forms";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [AppComponent],
+	imports: [BrowserModule, YesNoButtonGroupModule, ReactiveFormsModule],
+	providers: [],
+	bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
